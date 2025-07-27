319c34bc215c182899ac50e2e1e75fbfb80bf29b
yousefabodeif2000 <83887183+yousefabodeif2000@users.noreply.github.com>
2025-07-27 15:55:44 +0300

    added postgres config and fetch jobs

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -79,18 +79,21 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'jobtracker',          
+        'USER': 'postgres',             
+        'PASSWORD': '123456',     
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
 
+
 # Password validation
-# https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
